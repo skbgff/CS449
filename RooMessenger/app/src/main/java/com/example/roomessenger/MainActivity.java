2a680d0750469a800d6f04941abde477928e23e7
skbgff <47256558+skbgff@users.noreply.github.com>
2020-02-23 22:19:13 -0600

    RooMessenger

---
+++ RooMessenger/app/src/main/java/com/example/roomessenger/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.roomessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "Firebase is a success",Toast.LENGTH_LONG).show();
+    }
+}
